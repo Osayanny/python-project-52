533cd6e83318ece418a424661ec02c32ea99a646
Osayanny <osayanni7@gmail.com>
2025-02-03 17:52:06 +0500

    add eng localization

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -112,13 +112,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'task_manager', 'locale')
+]
 
 
 # Static files (CSS, JavaScript, Images)
