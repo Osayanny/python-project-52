c14c03fa3aea6d913732a2aab09caae4286efabe
Osayanny <osayanni7@gmail.com>
2025-02-24 14:46:17 +0500

    add custom login and logout views

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import View
-
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
 class IndexView(View):
 
@@ -8,3 +11,26 @@ class IndexView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
+
+
+class CustomLoginView(LoginView):
+
+    success_url = reverse_lazy('users_index')
+    success_message = _('You are logged in')
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            messages.success(request, self.success_message)
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+class CustomLogoutView(LogoutView):
+
+    success_url = 'users_index'
+    success_message = _('You are logged out')
+
+    def post(self, request, *args, **kwargs):
+        messages.success(request, self.success_message)
+        return super().post(request, *args, **kwargs)
