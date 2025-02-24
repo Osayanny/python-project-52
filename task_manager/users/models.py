e878a22bcb7c0e99e3cefb67aa72a2e8f4080b43
Osayanny <osayanni7@gmail.com>
2025-02-24 14:49:41 +0500

    Fix User model

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,10 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(models.Model):
-    username = models.CharField(max_length=255)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+class User(AbstractUser):
+
     created_at = models.DateTimeField(auto_now_add=True)
 
+
     def __str__(self):
-        return self.username
+        return str(self.username)
