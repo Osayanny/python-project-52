654811af207fc8e79676283a395beba3c0fade35
Osayanny <osayanni7@gmail.com>
2025-02-24 14:48:34 +0500

    add paths for CRUD

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from task_manager.users import views
 
 urlpatterns = [
     path('', views.UsersIndexView.as_view(), name='users_index'),
+    path('create/', views.UsersCreateFormView.as_view(), name='users_create'),
+    path('<int:pk>/update', views.UserUpdateFormView.as_view(), name='users_update'),
+    path('<int:pk>/delete', views.UserDeleteFormView.as_view(), name='users_delete'),
 ]
