38925c346c8fa19ad2cd751c9074ff723c793296
Mikhail <osyanni7@gmail.com>
2025-03-11 12:23:08 +0500

    rewrite inheritence

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,54 +1,34 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 
 from .models import User
 from .forms import UserRegisterForm, UserUpdateForm
-from django.contrib import messages
+
 from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
+
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
+from task_manager.mixins import AuthenticationRequiredMixin, AuthorizationRequiredMixin
 
 
 
-class CustomLoginRequiredMixin(LoginRequiredMixin):
-
-    access_denied_message = _('You are not authorized! Please log in.')
-
-    def dispatch(self, request, *args, **kwargs):
-        current_user = self.get_object()
-
-        if not request.user.is_authenticated:
-            messages.error(request, self.access_denied_message)
-            return redirect('login')
-        
-        if request.user.username != current_user.username:
-            messages.error(request, self.permission_denied_message)
-            return redirect('users_index')
-            
-        return super().dispatch(request, *args, **kwargs)
-
-class UsersIndexView(ListView):
+class IndexView(ListView):
 
     model = User
     template_name = 'users/index.html'
 
-    def get(self, request, *args, **kwargs):
-        users = User.objects.all()
-        return render(request, self.template_name, {'users': users})
 
-
-class UsersCreateFormView(SuccessMessageMixin, CreateView):
+class CreateFormView(SuccessMessageMixin, CreateView):
 
     model = User
     form_class = UserRegisterForm
     template_name = 'users/create.html'
     success_url = reverse_lazy('login')
     success_message = _('User was added successfully')
-    
 
 
-class UserUpdateFormView(CustomLoginRequiredMixin, SuccessMessageMixin, UpdateView):
+
+class UpdateFormView(AuthenticationRequiredMixin, AuthorizationRequiredMixin, SuccessMessageMixin, UpdateView):
 
     model = User
     form_class = UserUpdateForm
@@ -56,12 +36,14 @@ class UserUpdateFormView(CustomLoginRequiredMixin, SuccessMessageMixin, UpdateVi
     success_url = reverse_lazy('users_index')
     success_message = _('User was updated successfully')
     permission_denied_message = _('You do not have permission to update another user.')
+    permission_denied_url_name = 'users_index'
 
 
-class UserDeleteFormView(CustomLoginRequiredMixin, SuccessMessageMixin, DeleteView):
+class DeleteFormView(AuthenticationRequiredMixin, AuthorizationRequiredMixin, SuccessMessageMixin, DeleteView):
 
     model = User
     template_name = 'users/delete.html'
     success_url = reverse_lazy('users_index')
     success_message = _('User was deleted successfully')
     permission_denied_message = _('You do not have permission to delete another user.')
+    permission_denied_url_name = 'users_index'
