149c044f7abd0dd8eba6156e7378e5192600bf00
Mikhail <osyanni7@gmail.com>
2025-03-11 12:21:16 +0500

    url patterns for statuses

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from task_manager.users import views
 
 urlpatterns = [
-    path('', views.UsersIndexView.as_view(), name='users_index'),
-    path('create/', views.UsersCreateFormView.as_view(), name='users_create'),
-    path('<int:pk>/update', views.UserUpdateFormView.as_view(), name='users_update'),
-    path('<int:pk>/delete', views.UserDeleteFormView.as_view(), name='users_delete'),
+    path('', views.IndexView.as_view(), name='users_index'),
+    path('create/', views.CreateFormView.as_view(), name='users_create'),
+    path('<int:pk>/update', views.UpdateFormView.as_view(), name='users_update'),
+    path('<int:pk>/delete', views.DeleteFormView.as_view(), name='users_delete'),
 ]
