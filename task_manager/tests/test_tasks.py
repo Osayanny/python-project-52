499bed3a4fdcfd6c6b6e16933f0c8f07a30ce05d
Mikhail <osyanni7@gmail.com>
2025-03-14 15:51:56 +0500

    make tests for tasks

---
+++ task_manager/tests/test_tasks.py
@@ -0,0 +1,102 @@
+from django.test import TestCase
+from task_manager.users.models import User
+from task_manager.tasks.models import Task
+from django.urls import reverse_lazy
+
+
+
+class AnonymousUserTestCase(TestCase):
+
+    def test_create(self):
+        response = self.client.get('/tasks/create/')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('login'))
+
+    def test_read(self):
+        response = self.client.get('/tasks/')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('login'))
+    
+    def test_update(self):
+        response = self.client.get('/tasks/1/update/')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('login'))
+
+    def test_delete(self):
+        response = self.client.get('/tasks/1/delete/')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('login'))
+
+
+class AuthenticatedUserTestCase(TestCase):
+
+    fixtures = ['users_for_tests', 'statuses_for_tests', 'tasks_for_tests']
+
+    def setUp(self):
+        user = User.objects.get(id=1)
+        self.client.force_login(user)
+
+    def test_task_create(self):
+        #get
+        response = self.client.get('/tasks/create/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tasks/create.html')
+
+        #Create task
+        response = self.client.post(
+            '/tasks/create/',
+            {
+                'name':'test_task',
+                'description':'test_description',
+                'status': 1,
+                'author': 1,
+                'executor': 2,
+                'created_at': "2025-03-03 14:28:00"
+            }
+        )
+        task = Task.objects.last()
+        self.assertEqual(task.name, 'test_task')
+        self.assertEqual(Task.objects.count(), 3)
+        self.assertRedirects(response, reverse_lazy('tasks_index'))
+
+    def test_task_read(self):
+
+        response = self.client.get('/tasks/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tasks/index.html')
+
+    def test_status_update(self):
+        #get
+        task = Task.objects.get(id=1)
+        response = self.client.get('/tasks/1/update/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tasks/update.html')
+
+        #Update status
+        response = self.client.post(
+            '/tasks/1/update/',
+            {
+                'name':'updated_task',
+                'description':'updated_description',
+                'status': 1,
+                'author': 1,
+                'executor': 2,
+                'created_at': "2025-03-03 14:28:00"
+            }
+        )
+        task = Task.objects.get(id=1)
+        self.assertRedirects(response, reverse_lazy('tasks_index'))
+        self.assertEqual(task.name, 'updated_task')
+        
+    def test_status_delete(self):
+        #get
+        task = Task.objects.get(id=1)
+        url = f'/tasks/{task.id}/delete/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tasks/delete.html')
+
+        #Delete status
+        response = self.client.post('/tasks/1/delete/')
+        self.assertRedirects(response, reverse_lazy('tasks_index'))
+        self.assertEqual(Task.objects.count(), 1)
