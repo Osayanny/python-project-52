986108e6923cd7a57c7717cf4795f4bfe879b6c3
Mikhail <osyanni7@gmail.com>
2025-03-17 15:02:18 +0500

    create filter ffor tasks

---
+++ task_manager/tasks/filters.py
@@ -0,0 +1,30 @@
+import django_filters
+from django import forms
+from .models import Task
+from ..labels.models import Label
+from django.utils.translation import gettext_lazy as _
+
+class TaskFilter(django_filters.FilterSet):
+
+    def show_own_tasks(self, queryset, arg, value):
+        if value:
+            return queryset.filter(author=self.request.user)
+        return queryset
+
+
+    own_tasks = django_filters.BooleanFilter(
+        method='show_own_tasks',
+        widget=forms.CheckboxInput,
+        label=_('Show own tasks')
+    )
+
+    labels = django_filters.ModelChoiceFilter(
+        queryset=Label.objects.all(),
+        widget=forms.Select
+    )
+
+    class Meta:
+        model = Task
+        fields = ['status', 'executor', 'labels']
+
+        
