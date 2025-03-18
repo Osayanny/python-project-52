b900344723bdc726f0e8992d1034fe30ec69d87c
Mikhail <osyanni7@gmail.com>
2025-03-18 16:22:52 +0500

    rewrite paths for apps

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -20,10 +20,10 @@ from task_manager import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.IndexView.as_view(), name='index'),
-    path('users/', include('task_manager.users.urls')),
-    path('statuses/', include('task_manager.statuses.urls')),
-    path('tasks/', include('task_manager.tasks.urls')),
-    path('labels/', include('task_manager.labels.urls')),
+    path('users/', include('task_manager.apps.users.urls')),
+    path('statuses/', include('task_manager.apps.statuses.urls')),
+    path('tasks/', include('task_manager.apps.tasks.urls')),
+    path('labels/', include('task_manager.apps.labels.urls')),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
 ]
