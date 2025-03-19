dca5d77df2d75a84e744ece495201086ef89140a
Mikhail <osyanni7@gmail.com>
2025-03-19 15:51:56 +0500

    add widgets and labels

--- task_manager/apps/labels/forms.py
+++ task_manager/apps/labels/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from .models import Label
+from django.utils.translation import gettext as _
+
 
 class LabelCreateForm(forms.ModelForm):
 
     class Meta:
         model = Label
         fields = ['name']
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder':_('Name')})
+        }
+
+        labels = {
+            'name': _('Name'),
+        }
--- task_manager/apps/tasks/filters.py
+++ task_manager/apps/tasks/filters.py
@@ -2,6 +2,8 @@ import django_filters
 from django import forms
 from .models import Task
 from ..labels.models import Label
+from ..users.models import User
+from ..statuses.models import Status
 from django.utils.translation import gettext_lazy as _
 
 class TaskFilter(django_filters.FilterSet):
@@ -20,11 +22,23 @@ class TaskFilter(django_filters.FilterSet):
 
     labels = django_filters.ModelChoiceFilter(
         queryset=Label.objects.all(),
-        widget=forms.Select
+        widget=forms.Select,
+        label = _('Labels')
+    )
+
+    executor = django_filters.ModelChoiceFilter(
+        queryset = User.objects.all(),
+        widget = forms.Select,
+        label = _('Executor')
+    )
+
+    status = django_filters.ModelChoiceFilter(
+        queryset = Status.objects.all(),
+        widget = forms.Select,
+        label = _('Status')
     )
 
     class Meta:
         model = Task
         fields = ['status', 'executor', 'labels']
-
         
