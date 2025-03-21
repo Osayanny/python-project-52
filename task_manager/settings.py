e5febd282a7076a257a945174515872bd214ca4d
Mikhail <osyanni7@gmail.com>
2025-03-21 14:29:16 +0500

    add postgree db

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 from django.contrib import messages
 from dotenv import load_dotenv
+import dj_database_url
 
 #Load variables ffom .env file
 load_dotenv()
@@ -102,10 +103,10 @@ ROLLBAR = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}'),
+        conn_max_age=600
+    )
 }
 
 
